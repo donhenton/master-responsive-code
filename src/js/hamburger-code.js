55955d00aecab4fab57c3d9944eb023068aadcd2
Don Henton <donhenton@gmail.com>
2017-03-31 15:14:10 -0500

    setting up for my own code 3

--- src/js/hamburger-code.js
+++ src/js/hamburger-code.js
@@ -1,3 +1,42 @@
- 
 
 
+
+$(function () {
+    //Set up the click behavior
+    $(".label-trigger").click(function () {
+        //Toggle the class .active on the hamburger icon
+        var trMenu = "215px";
+        var trBlock = '210px';
+        if ($(this).hasClass('active'))
+        {
+            trMenu = "10px";
+            trBlock = '0px';
+        }
+        
+        $(this).toggleClass("active");
+        $(this).animate({
+           
+            left: trMenu
+        }, 200, function () {
+            // Animation complete.
+        });
+        
+        $('.main-container').animate({
+           
+            left: trBlock
+        }, 200, function () {
+            // Animation complete.
+        });
+        
+    });
+});
+$(window).resize(function () {
+    //console.log($(window).width());
+    var w = $(window).width();
+    if (w > 640)
+    {
+        $(".label-trigger").removeClass('active');
+        $('#nav-trigger').attr('checked', false);
+    }
+
+});
